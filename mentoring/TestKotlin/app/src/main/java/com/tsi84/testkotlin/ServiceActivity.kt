52b2bcd9ee374709296d5c6b03441cef5a4bd388
seungil <chonrang84@gmail.com>
2022-04-09 13:23:29 +0900

    mentoring - Testkotlin - music service lottie sample animation update

--- mentoring/TestKotlin/app/src/main/java/com/tsi84/testkotlin/ServiceActivity.kt
+++ mentoring/TestKotlin/app/src/main/java/com/tsi84/testkotlin/ServiceActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 import com.tsi84.testkotlin.service.MusicService
 
 class ServiceActivity: BaseActivity(), View.OnClickListener {
@@ -12,6 +13,7 @@ class ServiceActivity: BaseActivity(), View.OnClickListener {
     var mBtnServiceStart: Button? = null
     var mBtnServiceStop: Button? = null
     var mBtnClose: Button? = null
+    var mViewAnim: LottieAnimationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +24,7 @@ class ServiceActivity: BaseActivity(), View.OnClickListener {
         mBtnServiceStop?.setOnClickListener(this)
         mBtnClose = findViewById(R.id.btn_close)
         mBtnClose?.setOnClickListener(this)
+        mViewAnim = findViewById(R.id.image_anim)
     }
 
     override fun onClick(v: View) {
@@ -36,11 +39,15 @@ class ServiceActivity: BaseActivity(), View.OnClickListener {
         // start MusicService
         val intent = Intent(this, MusicService::class.java)
         startService(intent)
+        // lottie play animation
+        mViewAnim?.playAnimation()
     }
 
     private fun stopMusicService() {
         // stop MusicService
         val intent = Intent(this, MusicService::class.java)
         stopService(intent)
+        // lottie pause animation
+        mViewAnim?.pauseAnimation()
     }
 }
